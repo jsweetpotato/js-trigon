dc7147bc6e36311b73344734124926d94d3b7ce1
jisu kim <lerry37@naver.com>
2021-04-05 21:52:49 +0900

    Message 1

--- app.js
+++ app.js
@@ -3,3 +3,20 @@ var song
 function preload() {
     song = loadSound("Bobby Richards.mp3");
 }
+
+function setup(){
+    cnv = createCanvas(windowWidth, windowHeight);
+    fill(100);
+}
+
+function draw(){
+    background(0);
+}
+
+function mouseClicked(){
+    if (song.isPlaying) {
+        song.pause();
+    } else {
+        song.play();
+    }
+}

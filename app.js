a403e5191f4b171f3cedc9f02122821a9124b6f2
jisu kim <lerry37@naver.com>
2021-04-06 17:53:14 +0900

    play btn + sound visualizer

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 var song
 var fft
-
+var button = document.querySelector(".playBtn");
 
 function preload() {
-    song = loadSound('19th Floor - Bobby Richards.mp3');
+    song = loadSound('music/Hologram - Bobby Richards.mp3');
     masterVolume(0.5);
 }
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
     angleMode(DEGREES);
+    song.loop();
     delay = new p5.Delay();
     fft = new p5.FFT(0.6, 2048);
 }
@@ -17,13 +18,13 @@ function setup(){
 function draw(){
     background(0);
     stroke(255);
-    strokeWeight(4);
+    strokeWeight(5);
     strokeJoin(ROUND);
     noFill();
 
    var wave = fft.analyze();
 
-    translate(width/2, height/2 -100);
+   translate(width/2, height/2);
 
    for(var t = -1; t <= 1; t+=2){
 
@@ -31,7 +32,7 @@ function draw(){
         for(var i = 0; i <= 180; i+= 0.5){
             var index = wave[i]
             
-            var r = map(index, 0, 1024, 150, 700);
+            var r = map(index, 0, 1024, 200, 700);
             
             var x = r * sin(i) * t;
             var y = r * cos(i);
@@ -40,14 +41,38 @@ function draw(){
         }
         endShape();
     }
+
+    stroke("rgba(255,255,255,0.5)");
+
+    for(var t = -1; t <= 1; t+=2){
+
+        beginShape();
+        for(var i = 0; i <= 180; i+= 0.5){
+            var index = wave[i]
+            
+            var r = map(index, 0, 256, 1200, 550);
+            
+            var x = r * sin(i) * t;
+            var y = r * cos(i);
+            vertex(x, y);
+            vertex(x * 3, y * 3)
+        }
+        endShape();
+    }
 }
 
-function mouseClicked(){
+function toggleSong(){
     if (song.isPlaying()) {
         song.pause();
-        delay.process(song, 0.1, .3, 1500);
+        delay.process(song, 0.1, .3, 15000);
+        button.value = "PLAY";
     } else {
         song.play();
         delay.process(song, 0, .0, 22050);
+        button.value = "PAUSE";
     }
+}
+
+if(button){
+    button.addEventListener("click", toggleSong);
 }
